206d04514ecc11ba2cb4b96fc5c676010b1b4f4c
Alexsa Tolentino <adt7577@nyu.edu>
2023-02-28 14:52:34 -0500

    Added Jingle

---
+++ BookBum/BookBum/BookBumApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookBumApp.swift
+//  BookBum
+//
+//  Created by Alexsa Tolentino on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct BookBumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
