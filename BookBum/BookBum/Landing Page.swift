2bdabf01316e096bbb613ba56148f189658232e9
Alexsa Tolentino <adt7577@nyu.edu>
2023-02-28 15:29:53 -0500

    nil

--- BookBum/BookBum/Landing Page.swift
+++ BookBum/BookBum/Landing Page.swift
@@ -29,59 +29,79 @@ struct Landing_Page: View {
     @State private var soundFile = bundleAudio[0]
     @State private var player: AVAudioPlayer? = nil
     var body: some View {
-    //Main
-     VStack {
-
-         
-//VStack1
-VStack {
-
-    
-//Vstack2
-VStack {
-    Image(uiImage: UIImage(named: "Logo.png")!)
-        .renderingMode(/*@START_MENU_TOKEN@*/.original/*@END_MENU_TOKEN@*/)
-        .resizable()
-        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-        .padding(.vertical)
-            .frame(width:340, height:340)
-                 
-        Text("BookBum © 2023")
-            .font(.caption)
-            .fontWeight(.bold)
-            .multilineTextAlignment(.center)
-            .font(.title)
-            .foregroundColor(.orange)
+        //Main
+        VStack {
+            
+            
+            //VStack1
+            VStack {
+                
+                
+                //Vstack2
+                VStack {
+                    Image(uiImage: UIImage(named: "Logo.png")!)
+                        .renderingMode(/*@START_MENU_TOKEN@*/.original/*@END_MENU_TOKEN@*/)
+                        .resizable()
+                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                        .padding(.vertical)
+                        .frame(width:340, height:340)
+                    
+                    Text("BookBum © 2023")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                        .font(.title)
+                        .foregroundColor(.orange)
                 } //Vstack2
-}//Vstack1
- 
-         
-HStack {
-        Button("Login") {
-                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-        }
-        .padding (40)
-        .buttonStyle(.bordered)
-        .buttonBorderShape(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=shape: ButtonBorderShape@*/.capsule/*@END_MENU_TOKEN@*/)
-        .hoverEffect(/*@START_MENU_TOKEN@*/.lift/*@END_MENU_TOKEN@*/)
-        .onTapGesture {
-            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
-        }
-        
-      Button("Register") {
-                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
-        .padding(40)
+            }//Vstack1
+            
+            
+            HStack {
+                Button("Login") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .padding (40)
+                .buttonStyle(.bordered)
+                .buttonBorderShape(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=shape: ButtonBorderShape@*/.capsule/*@END_MENU_TOKEN@*/)
+                .hoverEffect(/*@START_MENU_TOKEN@*/.lift/*@END_MENU_TOKEN@*/)
+                .onTapGesture {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                }
+                
+                Button("Register") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
+                .padding(40)
+                
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=shape: ButtonBorderShape@*/.capsule/*@END_MENU_TOKEN@*/)
+                .hoverEffect(/*@START_MENU_TOKEN@*/.lift/*@END_MENU_TOKEN@*/)
+            }
+            VStack {
+                HStack {
+                    Button("Play") {
+                        print("Button Play")
+                        player = loadBundleAudio(soundFile)
+                        print("player", player as Any)
+                        // Loop indefinitely
+                        player?.numberOfLoops = -1
+                        player?.play()
+                    }
+                    Button("Stop") {
+                        print("Button Stop")
+                        player?.stop()
+                    }
+                    Button("Next") {
+                        soundIndex = (soundIndex+1) % bundleAudio.count
+                        soundFile = bundleAudio[soundIndex];
+                    }
+                }
+            } //VStack Main
+        } // Some View
+    } //Content View
     
-        .buttonStyle(.borderedProminent)
-        .buttonBorderShape(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=shape: ButtonBorderShape@*/.capsule/*@END_MENU_TOKEN@*/)
-        .hoverEffect(/*@START_MENU_TOKEN@*/.lift/*@END_MENU_TOKEN@*/)
-             }
-     } //VStack Main
-    } // Some View
-  } //Content View
-
-struct Landing_Page_Previews: PreviewProvider {
-    static var previews: some View {
-        Landing_Page()
+    struct Landing_Page_Previews: PreviewProvider {
+        static var previews: some View {
+            Landing_Page()
+        }
     }
 }
